79558de7cad319fe311ab78f973f11717215d21c
ezCat <dennis_wittmer@yahoo.fr>
2016-09-29 23:55:08 +0200

    Update c#1

--- resources/views/coaching/calendar/index.blade.php
+++ resources/views/coaching/calendar/index.blade.php
@@ -2,4 +2,27 @@
 
 <input type="hidden" id="to-active" value="calendar">
 
-Yop
+<?php
+
+	for ($i=0; $i < 14; $i++) { 
+		echo "<div class='day' id=".date('d-m-y', strtotime("now + $i day"))."><h3>";
+		echo date('l j F', strtotime("now + $i day")), "</h3><br>";
+
+		if ($i == 2) {
+			echo "Aucune donnée à afficher";
+		}else{
+			echo "<div class='alert alert-info'>Evenement</div>";
+		}
+
+		echo "</div>";
+	}
+
+	## BOUCLE POUR LES 14J
+		## Ecrit la date
+		## Check si dans le tableau il y a des éléments à la date
+		## Si oui, Affiche la div avec le contenu
+		## Si non, affiche message "Aucune donnée"
+	## Fin de boucle 
+
+?>
+
--- resources/views/coaching/reminder/index.blade.php
+++ resources/views/coaching/reminder/index.blade.php
@@ -2,4 +2,27 @@
 
 <input type="hidden" id="to-active" value="reminder">
 
-Reminder
+<h2>Reminder</h2>
+
+<hr>
+
+<div class="alert alert-info">
+	<ul>
+		<li>X rappel(s) pour un jour</li>
+		<li>Les taches sont fixes, ce sera dans le calendrier que les taches ponctuelles seront affichées</li>
+		<li>Par exemple : 
+		<ul>
+			<li>Nous sommes Mardi :</li>
+			<li>Entrainement SM :</li>
+			<ul>
+				<li>Chaussures</li>
+				<li>Chaussetes x2</li>
+				<li>Short</li>
+				<li>Cuissard</li>
+				<li>Collant</li>
+				<li>Tshirt</li>
+			</ul>
+		</ul>
+		</li>
+	</ul>
+</div>
